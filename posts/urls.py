dd54454b6e47b6b305972dc8aa4bf2fa2de34c35
Gennady <vasilievga@yandex.ru>
2021-04-11 10:53:49 +0300

    For 1st review

--- posts/urls.py
+++ posts/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [path('new/', views.new_post, name="new_post"),
                path('<str:username>/<int:post_id>/edit/',
                     views.post_edit, name='post_edit'),
                # Профайл пользователя
-               path('<str:username>/<int:post_id>/', views.post_view, name='post'),
+               path('<str:username>/<int:post_id>/', views.post_view,
+                    name='post'),
                path("", views.index, name="index"),
                ]
